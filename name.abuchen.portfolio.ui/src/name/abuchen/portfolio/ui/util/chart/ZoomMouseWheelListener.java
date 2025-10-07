f6ad353911043e74fd7d8e63e16c5f8ffedff4ad
Stephan Wahlbrink <sw@wahlbrink.eu>
2025-10-07 11:39:53 +0200

    Add req of MOD1 for zoom in charts

--- name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/util/chart/ZoomMouseWheelListener.java
+++ name.abuchen.portfolio.ui/src/name/abuchen/portfolio/ui/util/chart/ZoomMouseWheelListener.java
@@ -11,22 +11,29 @@ public class ZoomMouseWheelListener implements Listener
 {
     private final static double ZOOM_RATIO = 0.1;
 
-    public static void attachTo(Chart chart)
+    public static void attachTo(Chart chart, boolean requireMod)
     {
-        Listener listener = new ZoomMouseWheelListener(chart);
-        chart.getPlotArea().getControl().addListener(SWT.MouseWheel, listener);
+        Listener listener = new ZoomMouseWheelListener(chart, requireMod);
+        chart.getPlotArea().getControl().addListener(SWT.MouseVerticalWheel, listener);
     }
 
     private final Chart chart;
 
-    private ZoomMouseWheelListener(Chart chart)
+    private final int requiredMod;
+
+    private ZoomMouseWheelListener(Chart chart, boolean requireMod)
     {
         this.chart = chart;
+        this.requiredMod = (requireMod) ? SWT.MOD1 : 0;
     }
 
     @Override
     public void handleEvent(Event event)
     {
+        if (!event.doit || (requiredMod != 0 && event.stateMask != requiredMod))
+            return;
+        event.doit = false;
+
         for (IAxis axis : chart.getAxisSet().getYAxes())
         {
             double coordinate = axis.getDataCoordinate(event.y);
